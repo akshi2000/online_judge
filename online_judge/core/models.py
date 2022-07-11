d66f00c6f6f915f87b6a3bb80370e02169c8758b
akshi2000 <awasthiakshit11@gmail.com>
2022-07-11 23:57:47 +0530

    Added required Models for the application

--- online_judge/core/models.py
+++ online_judge/core/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    correct_submissions = models.IntegerField(default=0)
+    incorrect_submissions = models.IntegerField(default=0)
+    runtime_error_submissions = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
+
+
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blogs")
+    name = models.CharField(max_length=20)
+    statement = models.TextField()
+    date = models.DateField(auto_now=True)
+    timestamp = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=50)
+    statement = models.TextField()
+    tags = models.ManyToManyField(Tag, blank=True, related_name="questions")
+    timelimit = models.DecimalField(max_digits=6, decimal_places=2, default=1)
+    memlimit = models.IntegerField(default=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Testcase(models.Model):
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="testcases"
+    )
+    testcase = models.TextField()
+    answer = models.TextField()
+
+    def __str__(self):
+        return self.question.name
+
+
+class Submission(models.Model):
+    submission_id = models.IntegerField(primary_key=True, auto_created=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="submissions")
+    ques = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name="submissions"
+    )
+    code = models.TextField()
+    lang = models.CharField(max_length=10)
+    verdict = models.CharField(max_length=50, default="Submission Queued")
+
+    def __str__(self):
+        return self.submission_id
